f7728702ed353f497eda5a39349202c64c7f6624
Rohilla <nipun.rohilla@accenture.com>
2018-06-07 18:01:16 +0530

    Flatmap changes

--- src/com/java8/streamsmapjava8/StringToUpperCase.java
+++ src/com/java8/streamsmapjava8/StringToUpperCase.java
@@ -1,8 +1,6 @@
 package com.java8.streamsmapjava8;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
